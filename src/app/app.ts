a2b0bab8e3af85fdf69e864b6f3a19058300b2d6
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-12-17 15:54:42 +0200

    about component ready

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Hero } from "./components/hero/hero";
 import { Navbar } from "./components/navbar/navbar";
+import { About } from "./components/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [Hero, Navbar],
+  imports: [Hero, Navbar, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
