0fc2950a3943d1556f1a76dd23b5a03651630af6
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-12-19 21:49:07 +0200

    floating labels created manually by 2-way binding & reusable component for inputs

--- src/app/components/field/field.ts
+++ src/app/components/field/field.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { IInput } from '../../interfaces/iinput';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-field',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './field.html',
   styleUrl: './field.css',
 })
 export class Field {
-  @Input() inputData!: IInput
-  getInputData() {
-    return this.inputData;
+  //todo the data recieved for each input from contact component -parent-
+  @Input() inputData!: IInput;
+  //todo catch input value
+  data: string = '';
+  //todo catch label
+  @ViewChild('floatingLabel') floatingLabel!: ElementRef;
+  onTyping(value: string): void {
+    const label = this.floatingLabel.nativeElement;
+    if (value && value.length > 0) {
+      label.classList.add('moving-up');
+      label.classList.remove('moving-down');
+    } else {
+      label.classList.add('moving-down');
+      label.classList.remove('moving-up');
+    }
   }
+
+
 }
